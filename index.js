5297f132c9af946bdd9eaa7cf126706c4fb7a2a1
Venika <venika.jarvis@gmail.com>
2021-04-22 21:30:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('text').textContent = 'This is really cool!';
+  }); 
+
+// both above and below are components to Node.textContent solution
+// Figure how to input this into function
+    // let text = document.getElementById('text').textContent;
